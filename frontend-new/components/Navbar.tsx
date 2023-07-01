737a2c59bf7ae22a03da1e05ea1c5050b986e406
Zoli Hanesz <hanesz.zoltan@gmail.com>
2023-07-01 13:26:37 +0100

    Initial navbar implementation

---
+++ frontend-new/components/Navbar.tsx
@@ -0,0 +1,33 @@
+// components/Navbar.tsx
+'use client'
+
+import { Separator } from "@/components/ui/separator"
+import Link from "next/link";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+
+const Navbar = () => {
+  return (
+    <div className="h-12">
+        <div className="flex items-center container mx-auto my-2">
+            <Link href="/" className="font-mono ml-2 flex-grow">Habit Tracker</Link>
+            <div className="mr-2 font-mono">
+                <DropdownMenu>
+                    <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Profile</DropdownMenuItem>
+                        <DropdownMenuItem>Billing</DropdownMenuItem>
+                        <DropdownMenuItem>Team</DropdownMenuItem>
+                        <DropdownMenuItem>Subscription</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+        </div>
+        <div className="container mx-auto">
+            <Separator className="h-0.5 bg-slate-200 my-2"/>
+        </div>
+    </div>
+  );
+};
+export default Navbar;
