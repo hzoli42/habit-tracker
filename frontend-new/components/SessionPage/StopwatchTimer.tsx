45e181e132e2d0a7fe98c399e318717e49318bf3
Zoli Hanesz <hanesz.zoltan@gmail.com>
2024-01-06 14:42:22 +0100

    Remove ugly timer attempt

--- frontend-new/components/SessionPage/StopwatchTimer.tsx
+++ frontend-new/components/SessionPage/StopwatchTimer.tsx
@@ -8,47 +8,47 @@ export type StopwatchTimerProps = {
     time: StopwatchTime
 }
 
-type StopWatchDigitProps = {
-    digit: number
-}
+// type StopWatchDigitProps = {
+//     digit: number
+// }
 
-function StopwatchDigit({digit}: StopWatchDigitProps) {
-    return (
-        <div className="flex flex-col grid grid-cols-1 ">
-            <div className="flex justify-center">
-                <Image
-                    src="/up-arrow.png"
-                    width={40}
-                    height={40} 
-                    alt="increment"
-                />
-            </div>
-            <div className="flex justify-center">
-                <p className="font-monomaniac-one text-[100px]">{digit}</p>
-            </div>
-            <div className="flex justify-center">
-                <Image
-                    src="/down-arrow.png" 
-                    width={40}
-                    height={40} 
-                    alt="decrement"
-                />
-            </div>
-        </div>
-    )
-}
+// function StopwatchDigit({digit}: StopWatchDigitProps) {
+//     return (
+//         <div className="flex flex-col items-center">
+//             <div>
+//                 <Image
+//                     src="/up-arrow.png"
+//                     width={40}
+//                     height={40} 
+//                     alt="increment"
+//                 />
+//             </div>
+//             <p className="font-mono text-[100px] p-0 m-0">{digit}</p>
+//             <div>
+//                 <Image
+//                     src="/down-arrow.png" 
+//                     width={40}
+//                     height={40} 
+//                     alt="decrement"
+//                 />
+//             </div>
+//         </div>
+//     )
+// }
 
 export default function StopwatchTimer({time} : StopwatchTimerProps) {
     return (
-        <div className="flex justify-center px-8 grid grid-cols-8 gap-0">
-            <StopwatchDigit digit={time.hours / 10}/>
-            <StopwatchDigit digit={time.hours % 10}/>
-            <div className="flex justify-center"><p className="font-monomaniac-one text-[100px]"> : </p></div>
-            <StopwatchDigit digit={time.minutes / 10}/>
-            <StopwatchDigit digit={time.minutes % 10}/>
-            <div className="flex justify-center"><p className="font-monomaniac-one text-[100px]"> : </p></div>
-            <StopwatchDigit digit={time.seconds / 10}/>
-            <StopwatchDigit digit={time.seconds % 10}/>
-        </div>
+        // <div className="flex items-center justify-center px-8 grid grid-cols-8 gap-0">
+        //     <StopwatchDigit digit={time.hours / 10}/>
+        //     <StopwatchDigit digit={time.hours % 10}/>
+        //     <div className="flex justify-center"><p className="font-mono text-[100px]"> : </p></div>
+        //     <StopwatchDigit digit={time.minutes / 10}/>
+        //     <StopwatchDigit digit={time.minutes % 10}/>
+        //     <div className="flex justify-center"><p className="font-mono text-[100px]"> : </p></div>
+        //     <StopwatchDigit digit={time.seconds / 10}/>
+        //     <StopwatchDigit digit={time.seconds % 10}/>
+        // </div>
+        <>
+        </>
     )
 }
