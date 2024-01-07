72e4f4ba30eb8af441d596d8e94acca454cf868a
Zoli Hanesz <hanesz.zoltan@gmail.com>
2024-01-07 17:25:35 +0100

    Further stylize stopwatch clock

--- frontend-new/components/SessionPage/StopwatchClock.tsx
+++ frontend-new/components/SessionPage/StopwatchClock.tsx
@@ -15,19 +15,29 @@ export enum StopwatchMode {
 }
 
 export default function StopwatchClock({time} : StopwatchTimerProps) {
-    const [mode, setMode] = useState<StopwatchMode>(StopwatchMode.Stopwatch)
+    function colouredDigits(digits: number) {
+        const firstDigit = digits / 10
+        const secondDigit = digits  % 10
+
+        return (
+            <>
+                {
+                    firstDigit == 0
+                    ? <p className="font-mono text-[100px] text-slate-200 px-1">{firstDigit}</p>
+                    : <p className="font-mono text-[100px] text-black-600 px-1">{firstDigit}</p>
+                }
+                {
+                    secondDigit == 0
+                    ? <p className="font-mono text-[100px] text-slate-200 px-1">{secondDigit}</p>
+                    : <p className="font-mono text-[100px] text-black-600 px-1">{secondDigit}</p>
+                }
+            </>
+        )
+
+
+    }
 
     return (
-        // <div className="flex items-center justify-center px-8 grid grid-cols-8 gap-0">
-        //     <StopwatchDigit digit={time.hours / 10}/>
-        //     <StopwatchDigit digit={time.hours % 10}/>
-        //     <div className="flex justify-center"><p className="font-mono text-[100px]"> : </p></div>
-        //     <StopwatchDigit digit={time.minutes / 10}/>
-        //     <StopwatchDigit digit={time.minutes % 10}/>
-        //     <div className="flex justify-center"><p className="font-mono text-[100px]"> : </p></div>
-        //     <StopwatchDigit digit={time.seconds / 10}/>
-        //     <StopwatchDigit digit={time.seconds % 10}/>
-        // </div>
         <Tabs defaultValue="stopwatch">
             <TabsList>
                 <TabsTrigger value="stopwatch">Stopwatch</TabsTrigger>
@@ -35,24 +45,18 @@ export default function StopwatchClock({time} : StopwatchTimerProps) {
             </TabsList>
             <TabsContent value="stopwatch">
                 <div className="flex justify-center items-end px-8">
-                    <p className="font-mono text-[100px]">{time.hours}</p> <p className="font-mono text-[50px] pr-8 py-4">h</p>
-                    <p className="font-mono text-[100px]">{time.minutes}</p> <p className="font-mono text-[50px] pr-8 py-4">m </p>
-                    <p className="font-mono text-[100px]">{time.seconds}</p> <p className="font-mono text-[50px] pr-8 py-4">s </p>
+                    {colouredDigits(time.hours)} <p className="font-mono text-[50px] pr-8 py-4">h</p>
+                    {colouredDigits(time.minutes)} <p className="font-mono text-[50px] pr-8 py-4">m </p>
+                    {colouredDigits(time.seconds)} <p className="font-mono text-[50px] pr-8 py-4">s </p>
                 </div>
             </TabsContent>
             <TabsContent value="timer">
                 <div className="flex justify-center items-end px-8">
-                    <p className="font-mono text-[100px]">{time.hours}</p> <p className="font-mono text-[50px] pr-8 py-4">h</p>
-                    <p className="font-mono text-[100px]">{time.minutes}</p> <p className="font-mono text-[50px] pr-8 py-4">m </p>
-                    <p className="font-mono text-[100px]">{time.seconds}</p> <p className="font-mono text-[50px] pr-8 py-4">s </p>
+                    {colouredDigits(time.hours)} <p className="font-mono text-[50px] pr-8 py-4">h</p>
+                    {colouredDigits(time.minutes)} <p className="font-mono text-[50px] pr-8 py-4">m </p>
+                    {colouredDigits(time.seconds)} <p className="font-mono text-[50px] pr-8 py-4">s </p>
                 </div>
             </TabsContent>
         </Tabs>
-
-        // <div className="flex justify-center px-8">
-        //     <p className="font-monomaniac-one text-[100px] p-0 m-0">
-        //         {time.hours > 9 ? "0" + time.hours : time.hours} : {time.minutes} : {time.seconds}
-        //     </p>
-        // </div>
     )
 }
