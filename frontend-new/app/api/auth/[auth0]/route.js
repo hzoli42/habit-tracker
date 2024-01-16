ce5b1b6c220736c8ae4917e4250255b6a90da7fc
Zoli Hanesz <hanesz.zoltan@gmail.com>
2024-01-16 12:01:30 +0100

    Redirect to tracking page after login

--- frontend-new/app/api/auth/[auth0]/route.js
+++ frontend-new/app/api/auth/[auth0]/route.js
@@ -2,6 +2,10 @@
 // app/api/auth/[auth0]/route.js
 
 
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+    login(req, res) {
+        return handleLogin(req, res, {returnTo:'/track'});
+    }
+});
--- frontend-new/app/page.tsx
+++ frontend-new/app/page.tsx
@@ -20,7 +20,6 @@ export type Action = {
 export default function Home() {
   const { user, error, isLoading } = useUser();
 
-  if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
   return (
