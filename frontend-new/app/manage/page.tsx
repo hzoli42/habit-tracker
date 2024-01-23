b9c071dbb659eb4ced6fea5ac10536cdfcc2e3d2
Zoli Hanesz <hanesz.zoltan@gmail.com>
2024-01-23 18:44:22 +0100

    Add line charts and display dummy data

--- frontend-new/app/manage/page.tsx
+++ frontend-new/app/manage/page.tsx
@@ -23,6 +23,41 @@ export type SessionResponse = {
     ]
 }
 
+const dummyData = [
+    {
+        date: new Date(1705964401000),
+        duration: 8
+    },
+    {
+        date: new Date(1705878001000),
+        duration: 4
+    },
+    {
+        date: new Date(1705791601000),
+        duration: 6
+    },
+    {
+        date: new Date(1705705201000),
+        duration: 5
+    },
+    {
+        date: new Date(1705618801000),
+        duration: 5
+    },
+    {
+        date: new Date(1705532401000),
+        duration: 7
+    },
+    {
+        date: new Date(1705446001000),
+        duration: 9
+    },
+    {
+        date: new Date(1705359601000),
+        duration: 2
+    }
+]
+
 export default function Home() {
     const { user, error, isLoading } = useUser();
     const [labels, setLabels] = useAtom(labelsAtom)
@@ -104,7 +139,7 @@ export default function Home() {
                         <DataTable data={data} columns={columns} />
                     </TabsContent>
                     <TabsContent value="analysis">
-                        <SessionAnalysisLineChart />
+                        <SessionAnalysisLineChart title="Total work time" data={dummyData} />
                     </TabsContent>
                 </Tabs>
             </div>
--- frontend-new/components/ManagePage/SessionAnalysisLineChart.tsx
+++ frontend-new/components/ManagePage/SessionAnalysisLineChart.tsx
@@ -1,17 +1,26 @@
 import { CardHeader } from "@mui/material";
-import { Card, CardContent, CardTitle } from "../ui/card";
+import { Card, CardContent, CardDescription, CardTitle } from "../ui/card";
+import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import { WidthFull } from "@mui/icons-material";
 
-export default function SessionAnalysisLineChart() {
+
+export default function SessionAnalysisLineChart({ title, data }: { title: string, data: { date: Date, duration: number }[] }) {
+    function formatDate(value: Date, index: number): string {
+        return `${value.getDay()}/${value.getMonth()}/${value.getFullYear()}`
+    }
+
     return (
-        <>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Total work time</CardTitle>
-                </CardHeader>
-                <CardContent>
-
-                </CardContent>
-            </Card>
-        </>
+        <Card>
+            <CardContent>
+                <CardTitle className="flex justify-center pt-8">{title}</CardTitle>
+                <ResponsiveContainer width="100%" aspect={3}>
+                    <LineChart data={data}>
+                        <Line type="monotone" dataKey="duration" stroke="#8884d8" />
+                        <XAxis dataKey="date" tickFormatter={formatDate} />
+                        <YAxis />
+                    </LineChart>
+                </ResponsiveContainer>
+            </CardContent>
+        </Card>
     )
 }
